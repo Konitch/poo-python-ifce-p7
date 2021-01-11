a9296b6fbc67ec289efb966669aeeb7c4e586407
Konitch <56593997+Konitch@users.noreply.github.com>
2021-01-10 21:18:17 -0300

    Commit

---
+++ Atividades/Atividade-06-linha.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import csv
+
+arquivo = r'C:\Users\Levi\Documents\IFCE\Programação Orientada a Objetos\poo-python-ifce-p7\Atividades\tabela-06.csv'
+funcionarios = []
+faltas = []
+
+with open(arquivo) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    count = 0
+    for row in csv_reader:
+            funcionarios.append(f'{row[0]}')
+            faltas.append(int(f'{row[1]}'))
+
+plt.plot(funcionarios, faltas)
+
+plt.title('Relação de Faltas dos Funcionários no Mês')
+plt.xlabel('Funcionários')
+plt.ylabel('Faltas/Mês')
+
+plt.show()
